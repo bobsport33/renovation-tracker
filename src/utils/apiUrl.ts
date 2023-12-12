c835feb812e3c5c6087af19684cd8a35e812bf99
bobsport33 <bob62kelly@gmail.com>
2023-12-12 16:43:38 -0600

    ud apiurl

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -1,13 +1,10 @@
-const BACKEND_ENV_VAR = process.env.BACKEND_ENV;
+const BACKEND_ENV: string | undefined = process.env.BACKEND_ENV;
 
-const apiUrl = (path: string) => {
-    console.log(path);
-    if (BACKEND_ENV_VAR === "production") {
-        console.log("production");
+const apiUrl = (path: string): string => {
+    if (BACKEND_ENV === "production") {
         return `https://renovation-estimator.netlify.app/api/${path}`;
-    } else {
-        return `http://localhost:3000/api/${path}`;
     }
+    return `http://localhost:3000/api/${path}`;
 };
 
 export default apiUrl;
