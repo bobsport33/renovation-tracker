8f714cc8d73fe91b2cf13f526afe29f3b6c10899
bobsport33 <bob62kelly@gmail.com>
2023-12-18 09:21:16 -0600

    remove cl

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -2,10 +2,9 @@ const BACKEND_ENV: string | undefined = process.env.NEXT_PUBLIC_BACKEND_ENV;
 
 const apiUrl = (path: string): string => {
     if (BACKEND_ENV === "production") {
-        console.log("prod");
         return `https://renovation-estimator.netlify.app/api/${path}`;
     }
-    console.log("local");
+
     return `http://localhost:3000/api/${path}`;
 };
 
